8f3b4a62f0786dc2e1051473d10e2407ed4b817b
NICKY B <Nickblackburn02@gmail.com>
2018-06-04 11:02:28 -0400

    added java server client

--- src/com/nicholasblackburn/javaclient.java
+++ src/com/nicholasblackburn/javaclient.java
@@ -1,4 +1,43 @@
 package com.nicholasblackburn;
 
+import java.io.*;
+import java.net.Socket;
+
 public class javaclient {
-}
+
+        public static void main(String args[]) throws IOException
+        {
+            long t1, t2;
+            while(true)
+            {
+                try{
+                    Socket soc = new Socket("localhost",2000);
+                    InputStreamReader isr = new InputStreamReader(System.in);
+                    BufferedReader br = new BufferedReader(isr);
+                    System.out.println("Type a string to ping : ");
+                    String str = br.readLine();
+                    OutputStream os = soc.getOutputStream();
+                    PrintWriter pw = new PrintWriter(os,true);
+                    InputStream in = soc.getInputStream();
+                    InputStreamReader inr = new InputStreamReader(in);
+                    BufferedReader br1 = new BufferedReader(inr);
+                    t1 = System.currentTimeMillis();
+                    pw.println(str);
+
+                    String str1 = br1.readLine();
+                    t2 = System.currentTimeMillis();
+                    System.out.println("Pinging "+soc.getInetAddress()+" with string "+str );
+                    System.out.println("Reply from "+soc.getInetAddress() +" String "+str1+" Length : "+str1.length());
+                    System.out.println("Sent : "+str.length()+" Received : "+str1.length()+" Lost : "+(str.length()-str1.length()));
+                    System.out.println("Approx. Time in Milliseconds  = "+(t2-t1));
+                }
+                catch(Exception e)
+                {
+                    System.out.println("Error : "+e.getMessage());
+                }
+            }
+
+        }
+
+    }
+
